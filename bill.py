51f63173895b7b58315ecdb01a28a57578530fc4
Surachai <toe@sr.com>
2019-12-08 02:27:59 +0700

    ...

--- bill.py
+++ bill.py
@@ -1,14 +1,26 @@
 from docx import Document
 from docx.shared import Mm, Pt
+import math
 
 class Person:
   def __init__(self):
     self.id = ""
     self.info = []
     self.total = []
+    self.cash = -1.0
 
+def split(delimiters, string, maxsplit=0):
+    import re
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    return re.split(regexPattern, string, maxsplit)
 
-f = open("/home/cobot/ddd/data.txt", "r")
+def normal_round(n):
+    if n - math.floor(n) < 0.5:
+        return math.floor(n)
+    return math.ceil(n)
+
+fname = "data_62_12_7"
+f = open(fname+".txt", "r")
 data = []
 for line in f.readlines():
   data.append(line)
@@ -33,6 +45,37 @@ for i in range(1, len(data)):
 for x in information:
   print(x.__dict__)
 
+for v in information:
+  _b_err = False
+  _b_cash = False
+  _sum_cash = 0.0
+  for _info in v.info:
+    if _info.find('*') != -1:
+      i=0
+      while i < len(_info):
+        if _info[i].isnumeric():
+          break
+        i=i+1
+      if i != 0:
+        new_info = split(["*","="], _info[i:])
+        new_info = [float(i) for i in new_info]
+        tmp_cash = new_info[0]*new_info[1]
+        #print("new=%s : old=%s : %s" %(new_info, _info, normal_round(new_info[0]*new_info[1])))
+        if new_info[1] > 4:
+          _b_cash = True
+          tmp_cash = normal_round(tmp_cash)
+          _sum_cash = _sum_cash+tmp_cash
+          #print("%s | %s" % (tmp_cash, _sum_cash))
+        if tmp_cash != new_info[2]:
+          print("new=%s : old=%s : %s" %(new_info, _info, normal_round(new_info[0]*new_info[1])))
+          _b_err = True
+          break
+          
+  if _b_err:
+    print("ERROR : %s" % v.__dict__)
+  elif _b_cash:
+    v.cash = _sum_cash
+
 document = Document()
 
 # page setup to A4
@@ -44,16 +87,19 @@ section.page_width = Mm(210)
 style = document.styles['Normal']
 font = style.font
 font.name = 'TH SarabunPSK'
-font.size = Pt(16)
+font.size = Pt(36)
 
 seq=seq+1
 for j in range(len(information)):
   p = document.add_paragraph('No.')
   p.add_run(str(seq))
-  p = document.add_paragraph('เลขสมาชิก : L')
+  p = document.add_paragraph('เลขสมาชิก : ')
+  if information[j].id[0].isnumeric():
+    p.add_run('L')
   p.add_run(str(information[j].id))
   document.add_paragraph(str('วันที่ : ')+str(date))
   document.add_paragraph(str('----------'))
+  line=6
   for k in information[j].info:
     i=0
     while i < len(k):
@@ -63,11 +109,22 @@ for j in range(len(information)):
     if i != 0:
       k = k[:i] + str(' ') + k[i:]
     document.add_paragraph(str(k))
+    line=line-1
   document.add_paragraph(str('----------'))
-  document.add_paragraph(str('รวม ')+str(information[j].total[0])+str(' ชิ้น'))
-  document.add_paragraph(str('เหลือ ')+str(information[j].total[1])+str(' ชิ้น'))
+  if not (information[j].total[0] == '-'):
+    document.add_paragraph(str('รวม ')+str(information[j].total[0])+str(' ชิ้น'))
+    line=line-1
+  if not (information[j].total[1] == '-'):
+    document.add_paragraph(str('เหลือ ')+str(information[j].total[1])+str(' ชิ้น'))
+    line=line-1
+  if information[j].cash != -1.0:
+    document.add_paragraph(str('ยอดชำระ ')+str(information[j].cash)+str(' บาท'))
+    line=line-1
+  while line > 0:
+    document.add_paragraph('')
+    line=line-1
   seq=seq+1
 
 
-document.save('demo.docx')
+document.save('demo_'+fname+'.docx')
 
