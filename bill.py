4a1196a546aefa66fa9a7713bd31003c9de925d3
Surachai Rodsai <surachairobotic@gmail.com>
2019-12-19 11:27:46 +0700

    ...

--- bill.py
+++ bill.py
@@ -6,8 +6,10 @@ class Person:
   def __init__(self):
     self.id = ""
     self.info = []
-    self.total = []
-    self.cash = -1.0
+    self.total = '-'
+    self.count = 0.0
+    self.cash = 0.0
+    self.old_total = 0.0
 
 def split(delimiters, string, maxsplit=0):
     import re
@@ -19,7 +21,7 @@ def normal_round(n):
         return math.floor(n)
     return math.ceil(n)
 
-fname = "data_62_12_8"
+fname = "data_62_12_17"
 f = open(fname+".txt", "r")
 data = []
 for line in f.readlines():
@@ -38,18 +40,26 @@ for i in range(1, len(data)):
   tmp = data[i].split()
   v = Person()
   v.id = tmp[0]
-  v.info = tmp[1:len(tmp)-2]
-  v.total = tmp[len(tmp)-2:]
+  v.info = tmp[1:len(tmp)-1]
+  v.total = tmp[len(tmp)-1:]
   information.append(v)
+
+infor_base = []
+
+#### To Do
+#### read database file to variable
+#f = open("database.txt", "r")
+#for t in f.readline():
+#  print(t)
+#  if 
 
 for x in information:
   print(x.__dict__)
 
 for v in information:
   _b_err = False
-  _b_cash = False
   _sum_cash = 0.0
-  print(v.info)
+  #print(v.info)
   for k in range(len(v.info)):
     if v.info[k].find('*') != -1:
       i=0
@@ -63,26 +73,51 @@ for v in information:
         mul = new_info[0]*new_info[1]
         #print("new=%s : old=%s : %s" %(new_info, _info, normal_round(new_info[0]*new_info[1])))
         if new_info[1] > 4:
-          _b_cash = True
           mul = normal_round(mul)
-          _sum_cash = _sum_cash+mul
+          v.cash = v.cash+mul
           #print("%s | %s" % (mul, _sum_cash))
+        else:
+          v.count = v.count + mul
         if (len(new_info) is 3) and (mul != new_info[2]):
           print("new=%s : old=%s : %s" %(new_info, _info, normal_round(new_info[0]*new_info[1])))
           _b_err = True
           break
         elif len(new_info) is 2:
-          print('In')
-          print(v.info[k])
+          #print('In')
+          #print(v.info[k])
           v.info[k] = v.info[k]+str('=')+str(mul)
-          print(v.info[k])
-          print('--In--')
-  print(v.info)
+          #print(v.info[k])
+          #print('--In--')
+    elif not (v.info[k][0] == 'ม'):
+      #print("debug : %s" % v.info[k])
+      i=0
+      while i < len(v.info[k]):
+        if v.info[k][i].isnumeric():
+          break
+        i=i+1
+      if i != 0:
+        #print("v.info[k][i:] : %s" % v.info[k][i:])
+        new_info = int(v.info[k][i:])
+        v.count = v.count + new_info
+  if v.count == int(v.count):
+    v.count = int(v.count)
+  if v.cash == int(v.cash):
+    v.cash = int(v.cash)
+    #print("v.count, int(v.count) : %f, %d" % (v.count, int(v.count)))
+  #print(v.info)
           
   if _b_err:
     print("ERROR : %s" % v.__dict__)
-  elif _b_cash:
-    v.cash = _sum_cash
+
+print('-----')
+for x in information:
+  print(x.__dict__)
+  
+#  for y in infor_base:
+
+#### To Do
+#### save variable to database file
+#f = open("database.tmp", "w")
 
 document = Document()
 
@@ -119,13 +154,17 @@ for j in range(len(information)):
     document.add_paragraph(str(k))
     line=line-1
   document.add_paragraph(str('----------'))
-  if not (information[j].total[0] == '-'):
-    document.add_paragraph(str('รวม ')+str(information[j].total[0])+str(' ชิ้น'))
+  if information[j].count != 0.0:
+    document.add_paragraph(str('รวม ')+str(information[j].count)+str(' ชิ้น'))
     line=line-1
-  if not (information[j].total[1] == '-'):
-    document.add_paragraph(str('เหลือ ')+str(information[j].total[1])+str(' ชิ้น'))
-    line=line-1
-  if information[j].cash != -1.0:
+    if not (information[j].total[0] == '-'):
+      old_total = float(information[j].total[0])
+      new_total = old_total-information[j].count
+      if new_total == int(new_total):
+        new_total = int(new_total)
+      document.add_paragraph(str('เหลือ ')+str(new_total)+str(' ชิ้น'))
+      line=line-1
+  if information[j].cash != 0.0:
     document.add_paragraph(str('ยอดชำระ ')+str(information[j].cash)+str(' บาท'))
     line=line-1
   while line > 0:
