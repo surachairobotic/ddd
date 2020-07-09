4223bdd6035978b31892274a1dba09befcde397c
Surachai Rodsai <surachairobotic@gmail.com>
2020-07-09 23:54:26 +0700

    ...

--- by_id.py
+++ by_id.py
@@ -7,19 +7,34 @@ fname = "database.txt"
 def main():
   vText = get_all_database()
 
+  #print(vText)
+  #vText = del_list_numpy(vText, 213)
+  #exit()
+
   f = open(str(fpath+"database_by_id.txt"), "w")  
-  for _id in range(0, 400):
+  for _id in range(0, 365):
     txt = "L"+"{:0>3d}".format(_id)
-    f.writelines("-----  "+txt+"  -----\n")
+    #f.writelines("-----  "+txt+"  -----\n")
     date = ""
     indx_remove = []
+    first=True
     for i in range(0, len(vText)):
+      #print("i/len(vText) : " + str(i) + "/" + str(len(vText)) + "id: " + str(_id))
       if vText[i].find("DATE") != -1:
         date = vText[i][:len(vText[i])-1]
-      if vText[i].find(txt) != -1:
-        if date != "":
-          f.writelines(date.replace("DATE : ", ""))
-        f.writelines(", "+vText[i])
+      if vText[i].find(txt) != -1 and vText[i].find("total") != -1:
+        if first:
+          if date != "":
+            f.writelines(date.replace("DATE : ", ""))
+          vText[i] = vText[i].replace("total", "เหลือ")
+          tmpTxt = list(filter(None, re.split(r',|\n|\t| ', vText[i])))
+          if tmpTxt[0] == "0000":
+            f.writelines("," + tmpTxt[0] + tmpTxt[1] + tmpTxt[2] + '\n')
+          if tmpTxt[0] == 'SPECIAL':
+            f.writelines("," + tmpTxt[3] + "ต่อ" + tmpTxt[5] + tmpTxt[7] + '\n')
+          else:
+            f.writelines("," + tmpTxt[1] + "," + tmpTxt[2] + '\n')
+          first=False
         indx_remove.append(i)
         date = ""
     vText = del_list_numpy(vText, indx_remove)
