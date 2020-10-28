449033e26d32f8e363076321f5fa5d3a871408d5
Surachai Rodsai <surachairobotic@gmail.com>
2020-10-28 22:46:43 +0700

    ...

--- bill.py
+++ bill.py
@@ -12,7 +12,7 @@ from docx.enum.style import WD_STYLE_TYPE
 #from pydrive.drive import GoogleDrive
 
 fpath = "data/"
-fname = "data_63_10_15"
+fname = "data_63_10_27"
 #fname = "data_63_09_26ชุดที่2"
 date = ""
 
