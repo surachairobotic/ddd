a0508ecf0e96d558713f40c8b9c9bfb041adaacb
Surachai Rodsai <surachairobotic@gmail.com>
2022-10-06 00:51:42 +0700

    reborn

--- convert.py
+++ convert.py
@@ -2,7 +2,7 @@ import convertapi
 
 class Convert:
     def DOCXtoPDF(self, docx, pdf):
-        convertapi.api_secret = 'oL7HYyv8aMGTQEtX'
+        convertapi.api_secret = 'igDzijoMcbjJ76If'
         result = convertapi.convert('pdf', { 'File': docx })
 
         # save to file
